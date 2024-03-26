200e4e95ecb5f13a5f0bedf7aedf595e47c0d8fe
Sarmad Raees Khan <raeessarmad200@gmail.com>
2024-03-26 17:54:15 +0500

    add constants

---
+++ constants/index.ts
@@ -0,0 +1,8 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_ENP_works', label: 'How ENP Works?' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
