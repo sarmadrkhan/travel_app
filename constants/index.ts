9ec2469966978a662ade92b2f7e203fafdec2bff
Sarmad Raees Khan <raeessarmad200@gmail.com>
2024-03-28 22:25:10 +0500

    .

--- constants/index.ts
+++ constants/index.ts
@@ -14,3 +14,72 @@ export const PEOPLE_URL = [
   '/person-3.png',
   '/person-4.png',
 ];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Real maps can be offline',
+    icon: '/map.svg',
+    variant: 'green',
+    description:
+      'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
+  },
+  {
+    title: 'Set an adventure schedule',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      "Schedule an adventure with friends. On holidays, there are many interesting offers from Hilink. That way, there's no more discussion",
+  },
+  {
+    title: 'Technology using augment reality',
+    icon: '/tech.svg',
+    variant: 'green',
+    description:
+      'Technology uses augmented reality as a guide to your hiking trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection',
+  },
+  {
+    title: 'Many new locations every month',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Lots of new locations every month, because we have a worldwide community of climbers who share their best experiences with climbing',
+  },
+];
+
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About ENP',
+      'Press Releases',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Sudhan trek', 'Rawalakot Masair', 'Muzaffarabad'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Mobile', value: '+39-331-7869641' },
+    { label: 'Email', value: 'raeessarmad200@gmail.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+  ],
+};
